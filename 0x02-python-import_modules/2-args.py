c9d90b98a46bfb8623030d797c6c2fa31e5cca95
anthoone <david.anthone49@gmail.com>
2024-05-06 11:28:18 +0100

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+#  Prints the number of and the list of its arguments
 if __name__ == "__main__":
     import sys
+    
     arg = sys.argv
     size = len(arg) - 1
     dote = ['.', ':']
