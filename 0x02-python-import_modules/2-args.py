7ef402fa5d21b23768c6429b5c867d895d567ad9
anthoone <david.anthone49@gmail.com>
2024-05-06 10:04:22 +0100

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
         dote = dote[0]
     elif a >= 2:
         dote = dote[1]
-
     print("{} arguments{}".format(a-1, dote))
-
     for index, item in enumerate(sys.argv[1:]):
         print("{}: {}".format(index+1, item))
