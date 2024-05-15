a05e83aac6f2ae9e106f104667b6dd63613c950d
SLAMI YASSINE <yassinx4002@gmail.com>
2024-05-15 20:55:15 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     operators = ['+', '-', '*', '/']
 
     if size != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     else:
@@ -17,10 +17,17 @@ if __name__ == "__main__":
         b = int(arg[3])
         operator = arg[2]
 
-        for i in operators:
-            if operator != i:
-                print("Unknown operator. Available operators: +, -, * and /\n")
-                exit(1)
-            else:
-                print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-                exit(0)
+        if operator == '+':
+            action = add
+        elif operator == '-':
+            action = sub
+        elif operator == '*':
+            action = mul
+        elif operator == '/':
+            action = div
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+        print("{} {} {} = {}".format(a, operator, b, action(a, b)))
+        exit(0)
