fb8de4e74f65f6d39d95e1e1511dd28beaf6119d
seelmacheg <chegdalisalma@gmail.com>
2024-05-20 10:15:36 -0700

    6-main.py

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -10,15 +10,3 @@ matrix = [
 print_matrix_integer(matrix)
 print("--")
 print_matrix_integer()
-
-''' 
-
-guillaume@ubuntu:~/0x03$ ./6-main.py | cat -e
-1 2 3$
-4 5 6$
-7 8 9$
---$
-$
-guillaume@ubuntu:~/0x03$ 
-
-'''
