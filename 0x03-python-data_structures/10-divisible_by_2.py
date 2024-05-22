afa95223fc317551b44fc89ffbab8cd95c2c1492
SLAMI YASSINE <yassinx4002@gmail.com>
2024-05-22 14:53:53 +0100

    10-divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
+    new_list: list[bool] = []
     for integer in my_list:
         new_list.append(False if integer % 2 else True)
     return new_list
