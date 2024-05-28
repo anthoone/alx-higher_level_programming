b2e43af40b0556ec0b8509bea200bb993db23e86
SLAMI YASSINE <yassinx4002@gmail.com>
2024-05-28 18:00:29 +0100

    0x04 first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n_matrix = matrix.copy()
+    n_matrix = []
     for i in range(len(matrix)):
         n_matrix[i] = list(map(lambda x: x**2, matrix[i]))
     return n_matrix
