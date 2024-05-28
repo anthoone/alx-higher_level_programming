8a54bd5db10240ada6d62e6e17fc461cdb47671c
SLAMI YASSINE <yassinx4002@gmail.com>
2024-05-28 16:38:40 +0100

    0x04 first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[(element ** 2) for element in row] for row in matrix]
+if __name__ == "__main__":
+    def square_matrix_simple(matrix=[]):
+        return [[element ** 2 for element in row] for row in matrix]
