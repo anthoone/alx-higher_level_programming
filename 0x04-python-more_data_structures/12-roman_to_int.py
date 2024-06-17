968a1f0bd2d53a174f2fc6404df67bf7c5d2735b
SLAMI YASSINE <yassinx4002@gmail.com>
2024-06-17 07:34:19 +0100

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
+def roman_to_int(roman_string: str) -> int:
+    lookup = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M" : 1000
+    }
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_num = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_num += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]]
+    N = len(roman_string)
+    i = N - 1
+    output = 0
+    while i >= 0:
+        if i < N - 1 and lookup[roman_string[i]] < lookup[roman_string[i + 1]]:
+            output -= lookup[roman_string[i]]
         else:
-            roman_num += roman_d[roman_string[j]]
-    return (roman_num)
+            output += lookup[roman_string[i]]
+        i -= 1
+    return (output)
