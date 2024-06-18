1c4ee90ecaf6eda187205e3fc91fb8edae740a52
seelmacheg <chegdalisalma@gmail.com>
2024-06-17 18:25:48 -0700

    square_matrix_map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda sub_list: list(map(lambda x:x ** 2, sub_list)), matrix))
+    return  list(map(lambda sub_list: list(map(lambda x:x ** 2, sub_list)), matrix))
